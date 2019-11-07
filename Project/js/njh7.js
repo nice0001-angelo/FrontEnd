a3f22eb1fa0bb67711b0639ec3f1a04cf07dff91
nice0001-angelo <nice0001@gmail.com>
2019-11-07 21:10:54 +0900

    update

--- Project/js/njh7.js
+++ Project/js/njh7.js
@@ -11,3 +11,13 @@ function changeColor() {
     var bodyTag = document.getElementById("theBody");
     bodyTag.style.backgroundColor = color[i];
 }
+
+function myFnc(name,area){
+    document.write("안녕하세요"+name+"입니다"+"<br />");
+    document.write("사는곳은"+area+"입니다."+"<br />");
+}
+
+
+myFnc("남진현","토론토");
+
+myFnc("노지영","토론토");
