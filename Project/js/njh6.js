d9fc6fb8cd9704c3bac1eebf68deff7d21f5e91d
nice0001-angelo <nice0001@gmail.com>
2019-11-07 15:31:19 +0900

    update

--- Project/js/njh6.js
+++ Project/js/njh6.js
@@ -1,8 +1,12 @@
-var userName = prompt("당신의 영문이름은","");
+var userName = prompt("당신의 영문이름은?", "");
 
 var upperName = userName.toUpperCase();
-document.write(upperName+"<br />");
+document.write(upperName + "<br />");
 
-var usrNum = prompt("당신의 연락처는","");
-var result = userNum.substring(0,userNum.length-4)+"****";
-document.write(result+"<br />");
+var userNum = prompt("당신의 연락처는?", "");
+var result = userNum.substring(0, userNum.length - 4) + "****";
+document.write(result + "<br />");
+
+var userEmail = prompt("당신의 이메일은","");
+var arrUrl = [".co.kr",".com",".net","or.kr","go.kr"];
+
