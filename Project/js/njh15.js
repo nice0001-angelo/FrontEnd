a6cec04a0e4e47df088e36011af0294b13cf9298
nice0001-angelo <nice0001@gmail.com>
2019-11-13 21:33:05 +0900

    update

---
+++ Project/js/njh15.js
@@ -0,0 +1,36 @@
+
+
+var sum =0;
+
+for(i = 0 ; i <= 10; i++){
+    sum = sum+i;
+    document.write("<p>1~10까지 sum ="+sum+"</p>")
+};
+
+
+
+document.write("<hr />")
+
+sum=0;
+
+for(i = 0 ; i <= 100; i++){
+    sum = sum+i;
+    document.write("<p>1~100까지 sum ="+sum+"</p>")
+};
+
+
+
+document.write("<hr />")
+           
+               
+a = 0;
+while(a < 5) {
+    document.write("<p> 뭔지 모르지만 반복하라 5번인듯??</p>")
+    a++;
+}
+
+ $( function() {
+    $( "#sortable" ).sortable();
+    $( "+#sortable" ).disableSelection();
+  } );
+
